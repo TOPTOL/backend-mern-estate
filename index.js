6d6fa690e1afaa23bdee03aa2abd5f8e4ba5817f
seye <ilesanmiseye@yahoo.com>
2023-12-29 14:16:55 +0100

    changes made to api

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js'
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
-        console.log('cConnected to MongoDB!');
+        console.log('Connected to MongoDB!');
     }).catch((err) => {
         console.log(err);
     });
@@ -15,9 +16,12 @@ mongoose
 
 const app = express();
 
-app.listen(3000,() => {
-    console.log('server is running on port 3000');
+app.use(express.json());
+
+app.listen(4000,() => {
+    console.log('server is running on port 4000');
     }
 );
 
 app.use('/api/user', userRouter);
+app.use('api/auth', authRouter);
